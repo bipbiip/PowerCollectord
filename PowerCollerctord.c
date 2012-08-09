4aef1180ada4316e3d9a741ff9d811bae1af7f0a
bipbiip <arnaud.vcb+github@gmail.com>
2012-08-09 21:55:56 +0200

    Ajout licence et debut modifications demon
    Preparation a ce que le demon puisse gere
    les erreurs de lectures sur le port serie

--- PowerCollerctord.c
+++ PowerCollerctord.c
@@ -1,3 +1,22 @@
+/*
+ *   powercollectord - a simple daemon to collect electric power 
+ *                     consumption from an electric meter
+ *   Copyright (C) 2012  bipbiip
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *  
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -12,16 +31,21 @@
 
 #define BAUDRATE B1200
 #define MODEMDEVICE "/dev/ttyUSB0"
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define _POSIX_SOURCE 1 
 #define FALSE 0
 #define TRUE 1
 #define DEBUG 0
 
-volatile int STOP=FALSE;
+int loopreader();
 
-main()
-{
+int main(){
+
+}
+
+
+int loopreader() {
   int fd, c, res, regex_err, regex_match;
+  int stop = FALSE;
   struct termios oldtio, newtio;
   char buf[255], debut[1],releve[46];
   char *PTEC,*HCHC,*HCHP,*IINST,*PAPP;
@@ -35,7 +59,10 @@ main()
 
 
   fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY );
-  if (fd <0) {perror(MODEMDEVICE); exit(.1); }
+  if (fd <0) {
+    perror(MODEMDEVICE); 
+	return(1); 
+  }
 
   tcgetattr(fd,&oldtio); /* save current port settings */
 
@@ -48,7 +75,7 @@ main()
   newtio.c_lflag = 0;
 
   newtio.c_cc[VTIME] = 0; /* inter.character timer unused */
-  newtio.c_cc[VMIN] = 170; /* blocking read until 5 chars received */
+  newtio.c_cc[VMIN] = 170; /* blocking read until 170 chars received */
 
   tcflush(fd, TCIFLUSH);
   tcsetattr(fd,TCSANOW,&newtio);
@@ -58,7 +85,7 @@ main()
     res = read(fd,debut,1);
   }
 
-  while (STOP==FALSE) {
+  while (stop == FALSE) {
     if (debut[0] == '\x02') {
       time(&now);
       res = read(fd,buf,255);
@@ -81,17 +108,19 @@ main()
         syslog(LOG_INFO,"%s",releve);
       }
       else {
-        while (debut[0] != '\x02') {
-            res = read(fd,debut,1);
-        }
+		stop = 2;
       }
     }
     else {
-      while (debut[0] != '\x02') {
-        res = read(fd,debut,1);
-      }
+      stop = 3;
     }
   }
+  free(preg);
+  free(releve);
   tcsetattr(fd,TCSANOW,&oldtio);
+  close(fd);
+  free(oldtio);
+  free(newtio);
+  return(stop);
 }
 
